0ad0729b7d063c686eb03b504a6d27e06636c99d
Axel Allain <axel.allain.a@gmail.com>
2021-03-06 08:17:08 +0100

    dotenv for Heroku

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -14,6 +14,7 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 import javax.security.auth.login.LoginException;
+import java.net.URISyntaxException;
 
 public class Bot {
 
@@ -29,7 +30,7 @@ public class Bot {
                 .build();
     }
 
-    public static void main(String[] args) throws LoginException, SchedulerException {
+    public static void main(String[] args) throws LoginException, SchedulerException, URISyntaxException {
         Bot b = new Bot();
         MyJda.setDefaultJda(b.jda);
         MyWaiter.setDefaultWaiter(b.waiter);
--- src/main/java/JdbcConfig.java
+++ src/main/java/JdbcConfig.java
@@ -1,6 +1,8 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,8 +11,11 @@ public class JdbcConfig {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(JdbcConfig.class);
 
-    public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/elliot", "postgres", "at22x")) {
+    public static void main(String[] args) throws URISyntaxException {
+        String dbUrl = "jdbc:postgresql://" + "ec2-52-50-171-4.eu-west-1.compute.amazonaws.com" + ':' + "5432/" + "d29dfj6gvlgjar" + "?sslmode=require";
+        String username = "fneyxnzadnmoxi";
+        String password = "8b4cddfd1245d9894f9e80046aee0f25d97654fb66488fc67dcd2d21790a3edf";
+        try (Connection connection = DriverManager.getConnection(dbUrl, username, password)) {
             LOGGER.info("Connected to PostgreSQL database!");
         } catch (SQLException e) {
             LOGGER.error("Connection failure.");
